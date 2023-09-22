53afe5261917b2aabe5cdc755eb9e01bb50ce4fd
Caio <caio2003@outlook.pt>
2023-09-22 10:16:26 -0300

    Adicionando scripts do Banco de Dados

---
+++ Banco de Dados -  Scripts/bd-api2023-2 query.sql
@@ -0,0 +1,15 @@
+use api_2023_2;
+
+select * from usuario;
+select * from chamada;
+
+
+## Junção tabelas usuario e chamada (Vai mostrar tudo menos os ids de ambas as tabelas)
+select u.user_nome,u.user_email, c.cha_tema, c.cha_desc, c.cha_inicio, c.cha_final 
+	from usuario u, chamada c 
+		where c.user_id = u.user_id;
+
+## Selecionando somente as chamadas de um cliente em específico
+select u.user_nome,u.user_email, c.cha_tema, c.cha_desc, c.cha_inicio, c.cha_final 
+	from usuario u, chamada c 
+		where c.user_id = u.user_id and c.user_id = 1;
